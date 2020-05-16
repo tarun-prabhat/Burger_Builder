46025f981eb73f92e57edc732f7c251655642b38
tarun-prabhat <39223666+tarun-prabhat@users.noreply.github.com>
2020-05-16 18:57:32 +0530

    Order Summary Updated

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../hoc/aux';
+import classes from './Layout.module.css'
+
+const Layout =(props)=>(
+    <Aux>
+        <div>
+        Toolbar,Sidedrawer,Backdrop
+    </div>
+    <main className={classes.Content}>
+        {props.children}
+    </main>
+    </Aux>
+    
+);
+
+export default Layout;
+    
